c2ab1f1d12be79a2f94778771200fb17c7a31525
jinalvirani02 <jinal.virani@contentstack.com>
2025-02-24 15:03:54 +0530

    disbale live preview

--- src/pages/live-preview-demo.js
+++ src/pages/live-preview-demo.js
@@ -9,28 +9,28 @@ import Contentstack from "contentstack";
 // });
 
 const Stack = Contentstack.Stack({ 
-  "api_key": process.env.CONTENTSTACK_API_KEY || '', 
-  "delivery_token": process.env.CONTENTSTACK_DELIVERY_TOKEN || "",
-  "environment": process.env.CONTENTSTACK_ENVIRONMENT || "",
+  "api_key": process.env.CONTENTSTACK_API_KEY || 'blt91377ab7c766f7e9', 
+  "delivery_token": process.env.CONTENTSTACK_DELIVERY_TOKEN || "cs1e98b519fb972881c64b6546",
+  "environment": process.env.CONTENTSTACK_ENVIRONMENT || "dev",
   live_preview: {
-      preview_token: process.env.CONTENTSTACK_PREVIEW_TOKEN || "",
+      preview_token: process.env.CONTENTSTACK_PREVIEW_TOKEN || "cs2a27637db06d1984a3fbc071",
       enable: true,
       host: process.env.CONTENTSTACK_PREVIEW_HOST || 'rest-preview.contentstack.com'
     },
 });
 
-export async function getStaticProps() {
-  try {
-    const entry = Stack.ContentType("lyrics").Entry("blt2000c7a1a79aa2cb");
-    const result = await entry.toJSON().fetch();
-    return {
-      props: {result: result},
-  }
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    return null;
-  }
-}
+// export async function getStaticProps() {
+//   try {
+//     const entry = Stack.ContentType("lyrics").Entry("blt2000c7a1a79aa2cb");
+//     const result = await entry.toJSON().fetch();
+//     return {
+//       props: {result: result},
+//   }
+//   } catch (err) {
+//     console.error("Error fetching data:", err);
+//     return null;
+//   }
+// }
 
 export default function Home({result}) {
   return (
