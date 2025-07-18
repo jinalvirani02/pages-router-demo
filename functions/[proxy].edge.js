e96fa3675b819cc163a0e1ab2494ae4740e6b252
jinalvirani02 <jinal.virani@contentstack.com>
2025-07-18 16:56:45 +0530

    Add optin function

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,12 +1,12 @@
 export default async function handler(request, context) {
   const redirectHosts = [
-    'consent-test.devcontentstackapps.com',
+    'pages-router-demo-test.devcontentstackapps.com'
   ];
 
   const url = new URL(request.url);
   console.log(url.hostname, "1");
   if (redirectHosts.includes(url.hostname)) {
-    url.hostname = 'pages-router-demo-test.devcontentstackapps.com';
+    url.hostname = 'consent-test.devcontentstackapps.com';
     return Response.redirect(url.toString(), 308); // permanent redirect
   }
   console.log(url.hostname, "2");
