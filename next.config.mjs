1410340dbb46df7721116521c428145b4b556b85
jinalvirani02 <jinal.virani@contentstack.com>
2025-08-05 16:40:09 +0530

    remove cache header

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-              key: "cache-control",
-              value: "max-age=0, s-maxage=40, stale-while-revalidate=10"
-          },
-        ],
-      },
-    ];
-  },
   images:{
       remotePatterns: [
           {
