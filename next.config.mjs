c1b80b6d7ffce615c00f23cdac558009363c270b
jinalvirani02 <jinal.virani@contentstack.com>
2025-08-05 16:44:23 +0530

    Update headers to no cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+              key: "cache-control",
+              value: "private, no-cache, no-store, max-age=0, must-revalidate"
+          },
+        ],
+      },
+    ];
+  },
   images:{
       remotePatterns: [
           {
